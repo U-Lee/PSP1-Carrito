9184d1628aafb8c39fa735a839b5917edc76ba11
U-Lee <hecotlaloc@gmail.com>
2017-12-10 01:26:00 -0600

    carrito PSP1

---
+++ src/carritopelis/CarritoPelis.java
@@ -0,0 +1,18 @@
+package carritopelis;
+
+/**
+ *
+ * @author Omar Ulises Hernández Cervantes, Oswaldo Josue Hernández Juárez
+ */
+public class CarritoPelis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaPeliculas ventana = new VentanaPeliculas();
+        ventana.setTitle("Compra peliculas");
+        ventana.setVisible(true);
+    }
+
+}
